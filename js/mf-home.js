b631b5b412eea4526d90f13969228f8691bd43ee
Bill <bill@mentallyfriendly.com>
2015-02-13 17:10:08 +1100

    first commit

---
+++ js/mf-home.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	function setLandingPageSize() {
+		var width = $(window).width();
+		var height = $(window).height();
+
+		$('#landing-page').css({
+			// 'min-width': width,
+			'min-height': height
+		});
+
+		$('.chevron').css({
+			
+		})
+
+	}
+
+	$(window).resize(function() {
+		// setLandingPageSize();
+	}); 
+
+	// setLandingPageSize();
+});
